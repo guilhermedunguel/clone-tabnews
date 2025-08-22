40b49097e8770f09e9145352cc4885c2d8e587c8
Guilherme Dunguel <guilhermedunguel@gmail.com>
2025-08-22 21:33:42 +0000

    change video to image

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
 function Home() {
   return (
     <div style={{ textAlign: "center" }}>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=sHgfRSLBUVFssHbe&autoplay=1"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+      <img src="https://i.ytimg.com/vi/qWddjIwHmXw/maxresdefault.jpg"></img>
     </div>
   );
 }
