b407a9a9660239f62f0bb261ab4d1698cd9c58c8
Guilherme Dunguel <guilhermedunguel@gmail.com>
2025-08-22 21:09:03 +0000

    set video autoplay

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <iframe width="1920" height="1080" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ag-lJ-t5CQGmXcpP&amp;controls=0?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  return (
+    <div style={{ textAlign: "center" }}>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=sHgfRSLBUVFssHbe&autoplay=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 }
-
 export default Home;
